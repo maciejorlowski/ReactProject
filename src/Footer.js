4cb7cf03fbecef130dae5c0351b11b3678c42057
maciej <maciej.orlowski43@gmail.com>
2018-12-17 23:05:28 +0100

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,18 @@ class Footer extends Component {
     render() {
         return (
             <PanelFooter id="footer" >
-                <div className=""></div>
-                Footer
+                <div className="" align="center">
+                    <tr>
+                        <th> Temat Pracy: &ensp;</th>
+                        <th> Strona filmowa</th>
+                    </tr>
+                    <tr>
+                        <th>Wykonali: &ensp;</th>
+                        <th>Maciej Orłowski&ensp;</th>
+                        <th>Tomasz  Łoszczyk&ensp;</th>
+                        <th>Przemysław Niemirówko&ensp;</th>
+                    </tr>
+                </div>
             </PanelFooter>
 
         );
--- src/details.js
+++ src/details.js
@@ -4,6 +4,8 @@ import './css/App.css';
 import './css/details.css';
 import { Row} from "react-bootstrap";
 import Col from "react-bootstrap/es/Col";
+import {Button, ButtonToolbar, ControlLabel, FormControl, FormGroup, Modal, Panel, PanelGroup} from "react-bootstrap";
+
 
 
 class Details extends Component {
@@ -18,7 +20,8 @@ class Details extends Component {
         console.log(props.item);
 
         this.state ={
-            movie : props.item
+            movie : props.item,
+            showEdit: false,
         }
     }
 
@@ -26,14 +29,23 @@ class Details extends Component {
 
     }
 
+    updateTitleName(newtitle) {
+        this.state.movie.title = newtitle
+    }
+
+
+    open = (state, currentIndex) => {
+        this.setState({[state]: true})
+        this.setState({currentIndex})
+    }
 
     render() {
         return (
             <div className="details details-pos">
 
                 <Row >
-                    <Col md={6} mdOffset={3}>
-                        {this.state.movie.id}
+                    <Col md={10} mdOffset={3}>
+                        {/*{this.state.movie.id}*/}
                         {this.state.movie.title}
                         {this.state.movie.year}
                         {this.state.movie.link}
@@ -41,22 +53,57 @@ class Details extends Component {
 
                         {this.state.movie.shows
                             .map(show =>
-                            <div>
-                                {show.date}
-                                {show.unvailablePlaces}
-                                {show.availablePlaces}
-                                {"  "}
-                            </div>
+                                <div>
+                                    {show.date}
+                                    {show.unvailablePlaces}
+                                    {show.availablePlaces}
+                                    {"  "}
+                                </div>
 
                             )}
                         {this.state.movie.gallery
                             .map(item =>
                                 <img className="movie-photo" src={item.image}  />
-
-
                             )}
                     </Col>
                 </Row>
+
+                <ButtonToolbar>
+                    <Button bsStyle="default" onClick={(event) => this.open("showEdit", this.state.movie.id)}>Edit
+                        Recipe</Button>
+                </ButtonToolbar>
+
+
+                <Modal show={this.state.showEdit} onHide={this.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Recipe</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <FormGroup controlId="formBasicText">
+                            <ControlLabel>Movie Title</ControlLabel>
+                            <FormControl
+                                type="text"
+                                placeholder="Enter text"
+                                onChange={(event) => this.updateTitleName(event.target.value)}
+                                value={this.state.movie.title}>
+                            </FormControl>
+                        </FormGroup>
+                        {/*<FormGroup controlId="formControlTextarea">*/}
+                        {/*<ControlLabel>Ingredients</ControlLabel>*/}
+                        {/*<FormControl*/}
+                        {/*componentClass="textarea"*/}
+                        {/*onChange={(event) => this.updateIngredients(event.target.value.split(","), currentIndex)}*/}
+                        {/*placeholder="Enter Ingredients (Seperate by Columns)"*/}
+                        {/*value={recipes[currentIndex].ingredients}>*/}
+                        {/*</FormControl>*/}
+                        {/*</FormGroup>*/}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button bsStyle="primary" onClick={this.close}> Close</Button>
+                    </Modal.Footer>
+
+                </Modal>
 
             </div>
 
